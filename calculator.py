a6c734a43ef6119599f8b70c965b519384abb503
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 17:59:23 -0700

    Still need to account for addition with indices

--- calculator.py
+++ calculator.py
@@ -11,23 +11,48 @@ exit_condition_not_reached = True
 
 user_operation = raw_input(">>>")
 
+
 while exit_condition_not_reached:
     try:
 
         parts_of_input = user_operation.split(" ")
         operation = parts_of_input[0]
 
-        if operation == "+":
-            sum_of_input = add(parts_of_input[1], parts_of_input[2])
-            print sum_of_input
+        if len(parts_of_input) == 3:
 
-        elif operation == "-":
-            difference_of_input = subtract(parts_of_input[1], parts_of_input[2])
-            print difference_of_input
+            if operation == "+":
+                sum_of_input = add(parts_of_input[1], parts_of_input[2])
+                print sum_of_input
 
-        elif operation == "*":
-            product_of_input = multiply(parts_of_input[1], parts_of_input[2])
-            print product_of_input
+            elif operation == "-":
+                difference_of_input = subtract(parts_of_input[1], parts_of_input[2])
+                print difference_of_input
+
+            elif operation == "*":
+                product_of_input = multiply(parts_of_input[1], parts_of_input[2])
+                print product_of_input
+
+            elif operation == "/":
+                quotient_of_input = divide(parts_of_input[1], parts_of_input[2])
+                print quotient_of_input
+
+            elif operation == "pow":
+                power_of_input = power(parts_of_input[1],parts_of_input[2])
+                print power_of_input
+
+            elif operation == "mod":
+                mod_of_input = mod(parts_of_input[1], parts_of_input[2])
+                print mod_of_input
+
+        elif len(parts_of_input) == 2:    
+            
+            if operation == "square":
+                square_of_input = square(parts_of_input[1])
+                print square_of_input
+
+            elif operation == "cube":
+                cube_of_input = cube(parts_of_input[1])
+                print cube_of_input
         
         else:
             print "Sorry, I don't understand this operation. Please try again."
@@ -35,7 +60,5 @@ while exit_condition_not_reached:
         user_operation = raw_input(">>>")
 
     except IndexError: 
-        print "That won't work, please put a space between all characters. \n e.g. + 5 5"
-        exit_condition_not_reached = False
-        user_operation = raw_input(">>>")
-        exit_condition_not_reached = True
+         print "That won't work, please put a space between all characters. \n e.g. + 5 5"
+         user_operation = raw_input(">>>")
